53f91f5a47f626a9b077fbef83eb55b9c83e7dfc
Imants Volkovs <imants.volkovs@gmail.com>
2018-02-05 18:44:15 +0200

    rollback (duo app design)

--- src/Auctor/Auctor.App/src/main.ts
+++ src/Auctor/Auctor.App/src/main.ts
@@ -2,11 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import './extensions';
 import { AppModule } from './app';
+import './common-styles';
 
-//import './styles/if-styles/if-styles.less';
-import './styles/llu/llu.less';
-import './styles/app.less';
-//import './favicon.ico';
-
-enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
