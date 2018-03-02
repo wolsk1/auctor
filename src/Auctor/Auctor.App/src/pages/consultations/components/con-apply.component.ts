42d88121a9fa6b81cd6c30aa897596ca2e352a3e
Imants Volkovs <imants.volkovs@gmail.com>
2018-03-02 11:24:19 +0200

    Apply form base code

--- src/Auctor/Auctor.App/src/pages/consultations/components/con-apply.component.ts
+++ src/Auctor/Auctor.App/src/pages/consultations/components/con-apply.component.ts
@@ -13,17 +13,17 @@ import { Consultation } from '../../../models';
     template: require('./con-apply.component.jade')
 })
 export class ConApplyComponent implements OnInit {
-    cons: Consultation[];
-    @Input() lecturerId: string = '3e4fdadc-cad6-4475-9d0a-2d752a796ca8';
     constructor(
         private consultations: ConsultationService,
         private titleService: TitleService) {
         this.cons = [];
+        this.courseCode = 'TestCode'
     }
 
+    cons: Consultation[];
+    courseCode: string;
+
     public ngOnInit(): void {
-        this.titleService.setTitle('con-apply');
-        this.consultations.getConsultations(this.lecturerId).subscribe(
-            cons => this.cons = cons);
+        this.titleService.setTitle('con-apply');       
     }
 }
